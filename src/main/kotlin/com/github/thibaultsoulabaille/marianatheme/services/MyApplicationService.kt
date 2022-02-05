5b7200ce217ea2328d8e9408e418b76d6ec0c1d1
GitHub Action <action@github.com>
2022-02-05 11:46:41 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/thibaultsoulabaille/marianatheme/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.thibaultsoulabaille.marianatheme.services
+
+import com.github.thibaultsoulabaille.marianatheme.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
